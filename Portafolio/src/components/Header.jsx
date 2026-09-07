bd15886065dc8ff682ed070345a39852c397541a
Matias Sanchez <matiassanchez13422@gmail.com>
2026-09-07 18:19:55 -0300

    Se cambio el estilo del nav

--- Portafolio/src/components/Header.jsx
+++ Portafolio/src/components/Header.jsx
@@ -10,22 +10,32 @@ function Header() {
       <Navbar className="NavHeader p-3">
         <div className="d-flex align-items-center w-100 position-relative">
           <div>
-            <img src={foto} alt="" />
-            <Navbar.Brand href="#home" className="nombre">
-              Matias Sanchez
-            </Navbar.Brand>
+            <ul>
+              <li>
+                <Navbar.Brand href="#home" className="nombre">
+                  Matias Sanchez
+                </Navbar.Brand>
+              </li>
+              <li>
+                <p>Programador</p>
+              </li>
+            </ul>
           </div>
 
           <div className="position-absolute start-50 translate-middle-x">
             <Nav>
-              <Nav.Link href="#" className="active">Inicio</Nav.Link>
+              <Nav.Link href="#" className="active">
+                Inicio
+              </Nav.Link>
               <Nav.Link href="#features">Sobre mi</Nav.Link>
               <Nav.Link href="#pricing">Contactos</Nav.Link>
             </Nav>
           </div>
 
           <div className="ms-auto">
-            <Button className="BotonHeader" variant="outline-primary">Trabajemos juntos</Button>
+            <Button className="BotonHeader" variant="outline-primary">
+              Trabajemos juntos
+            </Button>
           </div>
         </div>
       </Navbar>
